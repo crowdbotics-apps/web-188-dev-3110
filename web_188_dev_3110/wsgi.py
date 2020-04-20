efd1d5f1c4f32ba767a34eb2d3bcd668ea715dc7
Crowdbotics <team@crowdbotics.com>
2020-04-20 12:26:05 +0000

    Update models

--- web_188_dev_3110/wsgi.py
+++ web_188_dev_3110/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_188_dev_3110.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_188_dev_3110.settings")
 
 application = get_wsgi_application()
